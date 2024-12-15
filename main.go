66a5682d95c1e5b42109b2eca8f59f338bdb81a3
Matt Krueger <mkrueger@rstms.net>
2024-12-14 23:41:24 -0700

    Bump version: 0.0.1 → 0.0.2

--- main.go
+++ main.go
@@ -20,7 +20,7 @@ import (
 
 *********************************************************************************************/
 
-const Version = "0.0.1"
+const Version = "0.0.2"
 
 var recordFile *os.File
 var started bool
